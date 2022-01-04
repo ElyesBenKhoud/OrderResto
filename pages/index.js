f2bc39642d14722e289c3efca992220e01558569
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2022-01-04 16:48:23 +0100

    navbar and footer styled

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Navbar from "./components/Navbar.jsx";
-import Footer from "./components/Footer.jsx";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Navbar />
       <Head>
         <title>Delivery Pizza</title>
         <meta
@@ -15,8 +11,6 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      home page
-      <Footer />
     </div>
   );
 }

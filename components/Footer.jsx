96fabbd4e5e3f794a77ad94c0b7e7093bd3b7890
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2022-01-04 18:10:34 +0100

    footer updates

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,36 +4,19 @@ import styles from "../styles/Footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.item}>
+      {/* <div className={styles.item}>
         <Image src="/img/bg.png" objectFit="cover" layout="fill" alt="" />
-      </div>
+      </div> */}
       <div className={styles.item}>
         <div className={styles.card}>
-          <h2 className={styles.motto}>
-            OH YES, WE DID.THE LAMA PIZZA, WELL BAKED SLICE OF PIZZA.
-          </h2>
+          <h2 className={styles.motto}>Delivery</h2>
         </div>
         <div className={styles.card}>
-          <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
+          <h1 className={styles.title}>FIND OUR Restaurant</h1>
           <p className={styles.text}>
-            1654 R. Don Road #304.
-            <br /> NewYork, 85022
-            <br /> (602) 867-1010
-          </p>
-          <p className={styles.text}>
-            2356 K. Laquie Rd #235.
-            <br /> NewYork, 85022
-            <br /> (602) 867-1011
-          </p>
-          <p className={styles.text}>
-            1614 E. Erwin St #104.
-            <br /> NewYork, 85022
-            <br /> (602) 867-1012
-          </p>
-          <p className={styles.text}>
-            1614 W. Caroll St #125.
-            <br /> NewYork, 85022
-            <br /> (602) 867-1013
+            Falaise Souani .
+            <br /> Monastir, 5000
+            <br /> (216) 52 779 617
           </p>
         </div>
         <div className={styles.card}>

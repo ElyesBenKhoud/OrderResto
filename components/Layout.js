a38de2b964258891645a00ea54679286b6bcdaee
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2022-01-04 16:53:41 +0100

    featured added without styling

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Featured from "./Featured";
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
+      <Featured />
       {children}
       <Footer />
     </>

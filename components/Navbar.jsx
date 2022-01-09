d2b7574d8b2dcdbca721f45682558df3567f4f86
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2022-01-09 11:52:00 +0100

    before adding paypal method  v1.06.20

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -20,7 +20,9 @@ const Navbar = () => {
         <ul className={styles.list}>
           <li className={styles.listItem}>Homepage</li>
           <li className={styles.listItem}>Products</li>
-          <li className={styles.listItem}>Menu</li>
+          <Link href="/cart" passHref>
+            <li className={styles.listItem}>Menu</li>
+          </Link>
           <Image src="/img/logo.png" alt="" width="160px" height="69px" />
           <li className={styles.listItem}>Events</li>
           <li className={styles.listItem}>Blog</li>
